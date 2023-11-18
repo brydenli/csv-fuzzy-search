ec8e6d7ad00bc5daf4ae79a803b1bbe05a49b5a3
Bryden Li <brydenli.97@gmail.com>
2023-11-18 12:15:59 -0700

    Refactor

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,9 @@
 const express = require('express');
 const os = require('os');
 const multer = require('multer');
-const csv = require("csvtojson/v2");
 const router = express.Router();
 const upload = multer({ dest: os.tmpdir() });
-
-const csvToJson = async (files) => {
-  const jsonArr = files.map(async (file) => {
-    return await csv().fromFile(file.path);
-  });
-  return await Promise.all(jsonArr);
-};
-
-const filterJson = (json) => {
-  return json.map((obj) => {
-    return Object.fromEntries(Object.entries(obj).filter(([_, o]) => o != ''));
-  });
-};
-
-function convertToCSV(arr) {
-  const arr2 = [Object.keys(arr[0])].concat(arr);
-  return arr2.map((obj) => {
-    return Object.values(obj).toString();
-  }).join('\n');
-};
+const { csvToJson, filterJson, convertToCsv } = require('../util');
 
 router.post('/convert', upload.array('csvFiles'), async (req, res) => {
   try {
@@ -41,10 +21,9 @@ router.post('/convert', upload.array('csvFiles'), async (req, res) => {
       dictObj[obj[ref1]] = obj[ref2];
     });
     const resFile = sortedArr[0].map((obj) => {
-      console.log(dictObj[obj[ref1]]);
       return { ...obj, [ref2]: dictObj[obj[ref1]] };
     });
-    const data = convertToCSV(resFile);
+    const data = convertToCsv(resFile);
     return res.status(200).json({ data });
   } catch (e) {
     return res.status(400);
--- util.js
+++ util.js
@@ -1,10 +1,27 @@
-const parse = require('csv-parse');
+const csv = require("csvtojson/v2");
 
-export const parseCsvToJson = async (fileData) => {
-  await parse(fileData, (err, data) => {
-    if (err) {
-      return null;
-    }
-    return data;
+const csvToJson = async (files) => {
+  const jsonArr = files.map(async (file) => {
+    return await csv().fromFile(file.path);
   });
+  return await Promise.all(jsonArr);
+};
+
+const filterJson = (json) => {
+  return json.map((obj) => {
+    return Object.fromEntries(Object.entries(obj).filter(([_, o]) => o != ''));
+  });
+};
+
+function convertToCsv(arr) {
+  const arr2 = [Object.keys(arr[0])].concat(arr);
+  return arr2.map((obj) => {
+    return Object.values(obj).toString();
+  }).join('\n');
+};
+
+module.exports = {
+  csvToJson,
+  filterJson,
+  convertToCsv
 }
